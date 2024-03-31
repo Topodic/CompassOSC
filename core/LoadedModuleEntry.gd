4c770883a3ae68e3b7402a445c3c11fa18688c8a
Topodic <43795565+Topodic@users.noreply.github.com>
2024-03-31 10:09:48 -0700

    Add base module info scene for loaded modules tab.

---
+++ core/LoadedModuleEntry.gd
@@ -0,0 +1,21 @@
+extends PanelContainer
+
+@onready var module_name_label : RichTextLabel = get_node("MarginContainer/VBoxContainer/HBoxContainer/ModuleNameLabel")
+@onready var module_description_label : RichTextLabel = get_node("MarginContainer/VBoxContainer/ModuleDescription")
+
+func set_git_repo(url : String):
+	module_name_label.text = module_name_label.text.replace("Module Name",
+		"[color=light_blue][url=" + url + "]Module Name[/url][/color]")
+	module_name_label.meta_clicked.connect(func(meta): OS.shell_open(str(meta)))
+
+func set_module_name(name : String):
+	module_name_label.text = module_name_label.text.replace("Module Name", name)
+
+func set_module_version(version : int):
+	module_name_label.text = module_name_label.text.replace("v0", "v" + str(version))
+
+func set_author_name(name : String):
+	module_name_label.text = module_name_label.text.replace("Author Name", name)
+
+func set_description(description : String):
+	module_description_label.text = description
