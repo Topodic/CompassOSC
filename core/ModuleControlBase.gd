095fb9eea87c81c498b5c2f5b423e736cf44bd3a
Topodic <43795565+Topodic@users.noreply.github.com>
2024-03-31 10:11:02 -0700

    Made ModuleControlBase more flexible, and support other ModuleControlBases

--- core/ModuleControlBase.gd
+++ core/ModuleControlBase.gd
@@ -2,19 +2,49 @@
 class_name ModuleControlBase
 extends PanelContainer
 
-## An array of paths to [i]relevant[/i] control nodes, ie sliders or input fields.
+## An array of paths to [i]relevant[/i] control nodes, ie sliders, input fields, or other ModuleControls.
 @export var control_nodepaths : Array[NodePath] = []
-## A dictionary of the actual controls linked to by the above.
-var controls = {}
+
+## Whether the controls you wish to track will be in a Dictionary or Array.
+## Dictionary will be more useful for controls with specific, known names,
+## while Arrays will be more useful when you just need an easily-iterable
+## list of the controls.
+@export_enum("Dictionary", "Array") var control_collection_type = "Dictionary"
+
+## A collection of the actual controls linked to by the above.
+var controls = null
 ## This control's respective module
 var module = null
 
-func initialize(module : CPMOSCModule):
-	self.module = module 
+var _initialized = false
 
-func _ready():
+func initialize(module : CPMOSCModule):
+	self.module = module
+	assert("control_collection_type" != null, "Control collection type not set!")
+
+	match control_collection_type:
+		"Dictionary":
+			controls = {}
+		"Array":
+			controls = []
+
+
 	for path in control_nodepaths:
+		# Just skip empty paths.
 		if !path:
 			continue
 		var node = get_node(path)
-		controls[node.name] = node
+		if node is ModuleControlBase:
+			node.initialize(module)
+		match control_collection_type:
+			"Dictionary":
+				controls[node.name] = node
+			"Array":
+				controls.append(node)
+	
+	print("Controls connected: " + str(controls))
+	_initialized = true
+	post_initialize()
+
+func post_initialize():
+	pass
