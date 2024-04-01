57e81dc3232ef6e6c71468f08fc15b84e3ea9f69
Topodic <43795565+Topodic@users.noreply.github.com>
2024-03-31 22:15:09 -0700

    Add a comment to sample for clarity.

--- samples/HeldItemDetector/HeldItemDetector.gd
+++ samples/HeldItemDetector/HeldItemDetector.gd
@@ -2,10 +2,6 @@ extends CPMOSCModule
 
 signal active_changed(active)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 var currently_active = false:
 	set(val):
 		currently_active = val
@@ -17,6 +13,8 @@ func on_message_received(address, argument):
 			var id = argument[0]
 			if id == "minecraft:redstone_block" and !currently_active:
 				currently_active = true
+				# This will affect an animation called
+				# osc:/sample/holding_redstone_block(0.0:1.0)
 				send_message("holding_redstone_block", true)
 			elif id != "minecraft:redstone_block" and currently_active:
 				currently_active = false
